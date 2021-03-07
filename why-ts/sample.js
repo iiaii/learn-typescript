f58c12cecb1e39059db7bf78fd59f7a72522dd8a
iiaii <jhun9409@naver.com>
2021-03-07 23:39:18 +0900

    타입스크립트 코드 가이드

---
+++ why-ts/sample.js
@@ -0,0 +1,19 @@
+// function add(a, b) {
+//   return a + b;
+// }
+
+// add(10, 20);
+
+// @ts-check
+
+/**
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ * 
+ * @return {number}
+ */
+function sum(a, b) {
+  return a + b;
+}
+
+sum(10, 20);
